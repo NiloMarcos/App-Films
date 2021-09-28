f199993a6a75c0082152ee802d4c9214a7b29077
Nilo Marcos <nilo.n2m15@gmail.com>
2021-09-28 19:15:00 -0300

    implement of Modal with description of API

---
+++ src/components/Details/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function Details(){
+  return(
+    <View style={styles.container}>
+
+      <View style={styles.ModalContainer}>
+        <TouchableOpacity style={styles.btnVoltar}>
+          <Text style={{color: '#FFF', fontSize: 16}}>Voltar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titulo}>Vingadores Ultimato</Text>
+        <Text style={styles.sinopse}>Sinopse...</Text>
+        <Text style={styles.description}>Sinopse Completa...</Text>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+    marginRight: 10,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  ModalContainer: {
+    width: '100%',
+    height: '80%',
+    backgroundColor: '#121212',
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5
+  },
+  btnVoltar: {
+    backgroundColor: '#e52245',
+    padding: 10,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5
+  },
+  titulo: {
+    textAlign: 'center',
+    color: '#FFF',
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  sinopse: {
+    color: '#FFF',
+    fontSize: 18,
+    marginBottom: 8,
+    marginLeft: 10
+  },
+  description: {
+    color: '#FFF',
+    marginLeft: 10,
+    marginRight: 10,
+  }
+});
--- src/components/ListFilms/index.js
+++ src/components/ListFilms/index.js
@@ -1,10 +1,67 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal } from 'react-native';
+
+import Details from '../Details';
 
 export default function ListFilms({ data }) {
+  const [visibleModal, setVisibleModal] = useState(false);
+
   return(
     <View>
-      <Text>{data.nome}</Text>
+      <View style={styles.card}>
+        <Text style={styles.titulo}>{data.nome}</Text>
+        <Image style={styles.image} source={{ uri: data.foto}} />
+        {/* <Text>{data.sinopse}</Text> */}
+
+        <View style={styles.areaBotao}>
+          <TouchableOpacity style={styles.botao} onPress={() => setVisibleModal(true)}>
+            <Text style={styles.botaoText}>Leia Mais</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Modal transparent={true} animationType="slide" visible={visibleModal} >
+        <Details />
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  card: {
+    backgroundColor: '#FFFFFF',
+    margin: 15,
+    elevation: 3
+  },
+  titulo: {
+    padding: 15,
+    fontSize: 18,
+  },
+  image: {
+    height: 250,
+    zIndex: 2
+  },
+  areaBotao: {
+    alignItems: 'flex-end',
+    marginTop: -45,
+    zIndex: 9
+  },
+  botao: {
+    width: 100,
+    height: 37,
+    backgroundColor: '#09a6ff',
+    opacity: 1,
+    padding: 8,
+    borderBottomLeftRadius: 8,
+    borderTopLeftRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoText: {
+    fontSize: 16,
+    color: '#FFF',
+  }
+})
